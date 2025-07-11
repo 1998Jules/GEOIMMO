a34a08511f2387f86d3f9085d1d97e1abb9e01b6
kambioss <kambia.pouwedeou@oss.org.tn>
2025-07-11 11:49:20 +0100

    test

--- geoimmo/settings.py
+++ geoimmo/settings.py
@@ -145,6 +145,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 STATIC_URL = "static/"
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
